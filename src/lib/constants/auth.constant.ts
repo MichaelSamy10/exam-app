3730fcc93f2d796808cbc9005c596ea1adb114e3
MichaelSamy10 <michaelsamy196@gmail.com>
2025-12-05 12:38:29 +0200

    Update NEXTAUTH_COOKIE logic to include localhost check in production

--- src/lib/constants/auth.constant.ts
+++ src/lib/constants/auth.constant.ts
@@ -1,4 +1,5 @@
 export const NEXTAUTH_COOKIE =
-  process.env.NODE_ENV === "production"
+  process.env.NODE_ENV === "production" &&
+  process.env.HOST?.includes("localhost")
     ? "__Secure-next-auth.session-token"
     : "next-auth.session-token";
