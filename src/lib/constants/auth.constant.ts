3a4e010e46fca2fa42824cee3c983776487b49a8
MichaelSamy10 <michaelsamy196@gmail.com>
2025-12-05 12:53:50 +0200

    Refactor NEXTAUTH_COOKIE assignment for clarity

--- src/lib/constants/auth.constant.ts
+++ src/lib/constants/auth.constant.ts
@@ -1,5 +1,5 @@
 export const NEXTAUTH_COOKIE =
   process.env.NODE_ENV === "production" &&
   process.env.HOST?.includes("localhost")
-    ? "__Secure-next-auth.session-token"
-    : "next-auth.session-token";
+    ? "next-auth.session-token"
+    : "__Secure-next-auth.session-token";
